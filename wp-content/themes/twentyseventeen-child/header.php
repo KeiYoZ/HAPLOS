795f63b90434cc031acdb0bbda3f895f8f7a6ea9
lucentyr00101 <sarcosredilbertranz@gmail.com>
2017-10-18 14:11:45 +0800

    added dot navigation

--- wp-content/themes/twentyseventeen-child/header.php
+++ wp-content/themes/twentyseventeen-child/header.php
@@ -14,11 +14,82 @@
 			$('a[href*="#"]') .not('[href="#"]') .not('[href="#0"]') .click(function(event) {if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname ) {var target = $(this.hash); target = target.length ? target : $('[name=' + this.hash.slice(1) + ']'); if (target.length) {event.preventDefault(); $('html, body').animate({scrollTop: target.offset().top }, 850, function() {var $target = $(target); $target.focus(); if ($target.is(":focus")) {return false; } else {$target.attr('tabindex','-1'); $target.focus(); }; }); } } });
 
 			//Navigation dot bar
+			$(document).ready(function(){
+				$.scrollTo = $.fn.scrollTo = function(x, y, options){
+					if (!(this instanceof $)) return $.fn.scrollTo.apply($('html, body'), arguments);
+					options = $.extend({}, {
+						gap: {
+							x: 0,
+							y: 0
+						},
+						animation: {
+							easing: 'swing',
+							duration: 600,
+							complete: $.noop,
+							step: $.noop
+						}
+					}, options);
+					return this.each(function(){
+						var elem = $(this);
+						elem.stop().animate({
+							scrollLeft: !isNaN(Number(x)) ? x : $(y).offset().left + options.gap.x,
+							scrollTop: !isNaN(Number(y)) ? y : $(y).offset().top + options.gap.y
+						}, options.animation);
+					});
+				};
+				var aChildren = $("nav li").children();
+				var aArray = [];
+				for (var i=0; i < aChildren.length; i++) {    
+					var aChild = aChildren[i];
+					var ahref = $(aChild).attr('href');
+					aArray.push(ahref);
+				}     
+				$(window).scroll(function(){
+					var windowPos = $(window).scrollTop();
+					var windowHeight = $(window).height();
+					var docHeight = $(document).height();
+					for (var i=0; i < aArray.length; i++) {
+						var theID = aArray[i];
+						var divPos = $(theID).offset().top; 
+						var divHeight = $(theID).height(); 
+						if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+							$("a[href='" + theID + "']").addClass("name-active");
+						} else {
+							$("a[href='" + theID + "']").removeClass("name-active");
+						}
+					}
+					if(windowPos + windowHeight == docHeight) {
+						if (!$("nav li:last-child a").hasClass("name-active")) {
+							var navActiveCurrent = $(".name-active").attr("href");
+							$("a[href='" + navActiveCurrent + "']").removeClass("name-active");
+							$("nav li:last-child a").addClass("name-active");
+						}
+					}
+				});
+				$("nav a").click(function(evn){
+					evn.preventDefault();
+					$('html,body').scrollTo(this.hash, this.hash);
+				});
+			});
+
 
 		}); //document.ready end
 	</script>
 </head>
 <body>
+	<!-- SIDEBAR -->
+	<div class="sidebar">
+		<nav class="sidebar-nav">
+			<ul>
+				<li><a href="#top" class="dot name-active"><span></span></a></li>
+				<li><a href="#ano_ang_haplos" class="dot"><span></span></a></li>
+				<li><a href="#hemophilia" class="dot"><span></span></a></li>
+				<li><a href="#contact_haplos" class="dot"><span></span></a></li>
+				<li><a href="#mga_ospital" class="dot"><span></span></a></li>
+			</ul>
+		</nav>
+	</div>
+	<!-- /sidebar -->
 	<!-- NAVBAR START -->
 	<div class="top-header" id="top">
 		<nav class="navbar navbar-inverse navbar-fixed-top">
