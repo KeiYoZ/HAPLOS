be92eff7b4d4422770131f80db4a8c730c511b9f
lucentyr00101 <sarcosredilbertranz@gmail.com>
2017-10-22 18:25:27 +0800

    fixed navbar links and added last section in home page

--- wp-content/themes/twentyseventeen-child/front-page-en.php
+++ wp-content/themes/twentyseventeen-child/front-page-en.php
@@ -5,11 +5,8 @@ Template Name: Front Page English
 ?>
 
 <?php 
-if(is_page(66)) {
+if(is_page(125)) {
 	get_header('en');
-}
-else {
-	get_header();
 }
 ?>
 
@@ -39,7 +36,7 @@ else {
 			</div>
 			<div class="col-md-6">
 				<p class="hip-desc"><?php the_field('section_1_description'); ?></p>
-				<input type="button" class="start-process" value="Puntahan ang proseso">
+				<input type="button" class="start-process" value="Go to process">
 			</div>
 		</div>
 	</div>
@@ -110,10 +107,52 @@ else {
 	<!-- Fourth section end -->
 
 	<!-- Fifth section start -->
-	<div class="section" id="mga_ospital" style="background-color: #609a82; padding-top:130px;">
+	<div class="section section-5-container" id="mga_ospital" style="background-color: #609a82; padding-top:80px;s">
 		<div class="row">
-			<div class="col-md-7 col-centered section-5-container">
+			<div class="col-md-7 col-centered">
 				<p class="text-center section-5-title"><?php the_field('section_5_title'); ?></p>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">CEBU CITY</p>
+					<p class="hospital-name">Hemophilia Center of Cebu,</p>
+					<p class="hospital-details">Perpetual Succour Hospital, Gorordo Avenue, Cebu City 6000</p>
+					<p class="doc">Dr.Lynda Mae Lepatan</p>
+				</div>
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">MANILA</p>
+					<p class="hospital-name">Hemophilia Treatment Center,</p>
+					<p class="hospital-details">P2/F Benavides Cancer Institute, University of Santo Tomas Hospital, Espana Boulevard, Manila 1014
+					</p>
+					<p class="contact-text">+632 731 3001 local 2680</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">DAVAO CITY</p>
+					<p class="hospital-name">Mindanao Hemophilia and Bleeding Disorder Treatment Center, </p>
+					<p class="hospital-details">Brokenshire Hospital, Madapo Hill, Davao del Sur, Davao City 8000</p>
+					<p class="doc">Dr. Jeannie B. Ong</p>
+					<p class="contact-text">+6382 305 3525 local 4502</p>
+				</div>
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header"><br></p>
+					<p class="hospital-name">Section of Pediatric Hematology-Oncology </p>
+					<p class="hospital-details">Cancer Institute, Room 112, UPCM-Philippine General Hospital, Taft Avenue 1000, Manila</p>
+					<p class="contact-text">+632 554 8400 local 4121</p><div class="clearfix"></div>
+					<p class="contact-text"> +63 943 278 8800</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">QUEZON CITY</p>
+					<p class="hospital-name">Cancer and Hematology Center, </p>
+					<p class="hospital-details">Mezzanine, Philippine Children’s Medical Center, Quezon Avenue, Quezon City </p>
+					<p class="contact-text">+632 924 6601 local 314</p>
+				</div>
 			</div>
 		</div>
 	</div>
--- wp-content/themes/twentyseventeen-child/front-page.php
+++ wp-content/themes/twentyseventeen-child/front-page.php
@@ -2,9 +2,6 @@
 if(is_page(2)) {
 	get_header();
 }
-else {
-	get_header('en');
-}
 ?>
 
 <!-- dot nav start -->
@@ -79,7 +76,7 @@ else {
 	<!-- Third section end -->
 
 	<!-- Fourth section start -->
-	<div class="section" id="contact_haplos" style="background-color: #609a82; padding-top:120px;">
+	<div class="section" id="contact_haplos" style="background-color: #609a82; padding-top:120px; padding-bottom:200px;">
 		<div style="margin:0 auto;">
 			<div class="row">
 				<div class="col-md-12">
@@ -104,10 +101,52 @@ else {
 	<!-- Fourth section end -->
 
 	<!-- Fifth section start -->
-	<div class="section" id="mga_ospital" style="background-color: #609a82; padding-top:130px;">
+	<div class="section section-5-container" id="mga_ospital" style="background-color: #609a82; padding-top:80px;s">
 		<div class="row">
-			<div class="col-md-7 col-centered section-5-container">
+			<div class="col-md-7 col-centered">
 				<p class="text-center section-5-title"><?php the_field('section_5_title'); ?></p>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">CEBU CITY</p>
+					<p class="hospital-name">Hemophilia Center of Cebu,</p>
+					<p class="hospital-details">Perpetual Succour Hospital, Gorordo Avenue, Cebu City 6000</p>
+					<p class="doc">Dr.Lynda Mae Lepatan</p>
+				</div>
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">MANILA</p>
+					<p class="hospital-name">Hemophilia Treatment Center,</p>
+					<p class="hospital-details">P2/F Benavides Cancer Institute, University of Santo Tomas Hospital, Espana Boulevard, Manila 1014
+					</p>
+					<p class="contact-text">+632 731 3001 local 2680</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">DAVAO CITY</p>
+					<p class="hospital-name">Mindanao Hemophilia and Bleeding Disorder Treatment Center, </p>
+					<p class="hospital-details">Brokenshire Hospital, Madapo Hill, Davao del Sur, Davao City 8000</p>
+					<p class="doc">Dr. Jeannie B. Ong</p>
+					<p class="contact-text">+6382 305 3525 local 4502</p>
+				</div>
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header"><br></p>
+					<p class="hospital-name">Section of Pediatric Hematology-Oncology </p>
+					<p class="hospital-details">Cancer Institute, Room 112, UPCM-Philippine General Hospital, Taft Avenue 1000, Manila</p>
+					<p class="contact-text">+632 554 8400 local 4121</p><div class="clearfix"></div>
+					<p class="contact-text"> +63 943 278 8800</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-12">
+					<p class="treatment-header">QUEZON CITY</p>
+					<p class="hospital-name">Cancer and Hematology Center, </p>
+					<p class="hospital-details">Mezzanine, Philippine Children’s Medical Center, Quezon Avenue, Quezon City </p>
+					<p class="contact-text">+632 924 6601 local 314</p>
+				</div>
 			</div>
 		</div>
 	</div>
